f5595cfb19b3e8a7614bdaf5d84a68bc36051151
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-02-15 18:41:03 +0100

    First Complete Iteration

--- Laboration4/Form1.Designer.cs
+++ Laboration4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Laboration4
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startIntegrationButton = new System.Windows.Forms.Button();
+            this.integrationInProgressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startIntegrationButton
+            // 
+            this.startIntegrationButton.Location = new System.Drawing.Point(117, 32);
+            this.startIntegrationButton.Name = "startIntegrationButton";
+            this.startIntegrationButton.Size = new System.Drawing.Size(146, 23);
+            this.startIntegrationButton.TabIndex = 0;
+            this.startIntegrationButton.Text = "Starta integrering";
+            this.startIntegrationButton.UseVisualStyleBackColor = true;
+            this.startIntegrationButton.Click += new System.EventHandler(this.StartIntegrationButton_Click);
+            // 
+            // integrationInProgressLabel
+            // 
+            this.integrationInProgressLabel.AutoSize = true;
+            this.integrationInProgressLabel.Location = new System.Drawing.Point(142, 70);
+            this.integrationInProgressLabel.Name = "integrationInProgressLabel";
+            this.integrationInProgressLabel.Size = new System.Drawing.Size(96, 13);
+            this.integrationInProgressLabel.TabIndex = 1;
+            this.integrationInProgressLabel.Text = "Integrering pågår...";
+            this.integrationInProgressLabel.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 111);
+            this.Controls.Add(this.integrationInProgressLabel);
+            this.Controls.Add(this.startIntegrationButton);
+            this.MaximumSize = new System.Drawing.Size(420, 150);
+            this.MinimumSize = new System.Drawing.Size(420, 150);
+            this.Name = "MainForm";
+            this.Text = "Media Integrator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startIntegrationButton;
+        private System.Windows.Forms.Label integrationInProgressLabel;
     }
 }
 
--- Laboration4/Form1.cs
+++ Laboration4/Form1.cs
@@ -1,20 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Laboration4
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void StartIntegrationButton_Click(object sender, EventArgs e)
+        {
+            integrationInProgressLabel.Visible = true;
+
+            string directory = Directory.GetCurrentDirectory() + @"\frMediaShop";
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher()
+            {
+                Path = directory,
+                Filter = "*.csv",
+                NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite
+            };
+            fileSystemWatcher.Created += FileCreatedOrChanged;
+            fileSystemWatcher.Changed += FileCreatedOrChanged;
+            fileSystemWatcher.EnableRaisingEvents = true;
+
+            string reverseDirectory = Directory.GetCurrentDirectory() + @"\frSimpleMedia";
+            FileSystemWatcher reverseFileSystemWatcher = new FileSystemWatcher()
+            {
+                Path = reverseDirectory,
+                Filter = "*.xml",
+                NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite
+            };
+            reverseFileSystemWatcher.Created += ReverseFileCreatedOrChanged;
+            reverseFileSystemWatcher.Changed += ReverseFileCreatedOrChanged;
+            reverseFileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileCreatedOrChanged(object sender, FileSystemEventArgs e)
+        {
+            CreateXMLFromCsv();
+        }
+
+        private void ReverseFileCreatedOrChanged(object sender, FileSystemEventArgs e)
+        {
+            CreateCsvFromXML();
+        }
+
+        private void CreateXMLFromCsv()
+        {
+            string CSVFilePath = Directory.GetCurrentDirectory() + @"\frMediaShop\Products.csv";
+            string XMLFilePath = Directory.GetCurrentDirectory() + @"\tillSimpleMedia\Products.xml";
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true
+            };
+            XmlWriter xmlWriter = XmlWriter.Create(XMLFilePath, xmlWriterSettings);
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("Inventory");
+
+            using (StreamReader streamReader = new StreamReader(CSVFilePath))
+            {
+                string line;
+                string[] lineData;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    lineData = line.Split(';');
+                    xmlWriter.WriteStartElement("Item");
+
+                    xmlWriter.WriteStartElement("Name");
+                    xmlWriter.WriteString(lineData[1]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Count");
+                    xmlWriter.WriteString(lineData[6]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Price");
+                    xmlWriter.WriteString(lineData[2]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Comment");
+                    xmlWriter.WriteString("");
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Artist");
+                    xmlWriter.WriteString(lineData[4]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Publisher");
+                    xmlWriter.WriteString(lineData[5]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Genre");
+                    xmlWriter.WriteString("");
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("Year");
+                    xmlWriter.WriteString("0");
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("ProductID");
+                    xmlWriter.WriteString(lineData[0]);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteEndElement(); // </Item>
+                }
+            }
+
+            xmlWriter.WriteEndElement(); // </Inventory>
+            xmlWriter.Close();
+        }
+
+        private void CreateCsvFromXML()
+        {
+            string XMLFilePath = Directory.GetCurrentDirectory() + @"\frSimpleMedia\Products.xml";
+            string CSVFilePath = Directory.GetCurrentDirectory() + @"\tillMediaShop\Products.csv";
+            XDocument xDocument = XDocument.Load(XMLFilePath);
+            using (StreamWriter streamWriter = new StreamWriter(CSVFilePath))
+            {
+                foreach (XElement item in xDocument.Descendants("Item"))
+                {
+                    List<XElement> elements = item.Descendants().ToList();
+                    string product = elements.First(x => x.Name == "ProductID").Value + ";";
+                    product += elements.First(x => x.Name == "Name").Value + ";";
+                    product += elements.First(x => x.Name == "Price").Value + ";";
+                    product += "Ospecifierad;"; // Det finns ingen motsvarighet till varutyp från SimpleMedia.
+                    product += elements.First(x => x.Name == "Artist").Value + ";";
+                    product += elements.First(x => x.Name == "Publisher").Value + ";";
+                    product += elements.First(x => x.Name == "Count").Value + ";";
+                    product += "0"; // Det finns ingen motsvarighet till antal sålda från SimpleMedia.
+                    streamWriter.WriteLine(product);
+                }
+            }
         }
     }
 }
